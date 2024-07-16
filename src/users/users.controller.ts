b52c1e8b918b1850faf308c26ee9988923e975ee
Uday Kiran Kavaturu <udaykiran.k@zemosolabs.com>
2024-07-16 16:01:18 +0530

    chore: lint and format

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,43 +1,50 @@
 import {
-    Body,
-    Controller,
-    ForbiddenException,
-    ParseIntPipe,
-    Patch,
-    Query,
-    Request,
+  Body,
+  Controller,
+  ForbiddenException,
+  ParseIntPipe,
+  Patch,
+  Query,
+  Request,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDTO } from './dto';
 import { GetUser } from './get-user.decorator';
-import { ApiTags, ApiBadRequestResponse, ApiUnauthorizedResponse, ApiForbiddenResponse, ApiBearerAuth, ApiOkResponse } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiBadRequestResponse,
+  ApiUnauthorizedResponse,
+  ApiForbiddenResponse,
+  ApiBearerAuth,
+  ApiOkResponse,
+} from '@nestjs/swagger';
 import { COMMON_SWAGGER_RESPONSES, USER_SWAGGER_RESPONSES } from 'src/swagger';
 
 @ApiTags('users')
 @ApiBadRequestResponse({
-    example: COMMON_SWAGGER_RESPONSES.apiBadRequestResponse,
+  example: COMMON_SWAGGER_RESPONSES.apiBadRequestResponse,
 })
 @ApiUnauthorizedResponse({
-    example: COMMON_SWAGGER_RESPONSES.forbiddenResponse,
+  example: COMMON_SWAGGER_RESPONSES.forbiddenResponse,
 })
 @ApiBearerAuth()
 @Controller('users')
 export class UsersController {
-    constructor(private userService: UsersService) { }
+  constructor(private userService: UsersService) {}
 
-    @ApiForbiddenResponse({ example: USER_SWAGGER_RESPONSES.forbidden })
-    @ApiOkResponse({ example: USER_SWAGGER_RESPONSES.patch })
-    @Patch()
-    async updateUser(
-        @Query('id', ParseIntPipe) id: number,
-        @Body() updateUserDTO: UpdateUserDTO,
-        @GetUser() currentUser: any,
-        @Request() req
-    ) {
-        if (currentUser.sub !== id) {
-            throw new ForbiddenException('You can only update your own profile');
-        }
-
-        return await this.userService.update(id, updateUserDTO, req);
+  @ApiForbiddenResponse({ example: USER_SWAGGER_RESPONSES.forbidden })
+  @ApiOkResponse({ example: USER_SWAGGER_RESPONSES.patch })
+  @Patch()
+  async updateUser(
+    @Query('id', ParseIntPipe) id: number,
+    @Body() updateUserDTO: UpdateUserDTO,
+    @GetUser() currentUser: any,
+    @Request() req,
+  ) {
+    if (currentUser.sub !== id) {
+      throw new ForbiddenException('You can only update your own profile');
     }
+
+    return await this.userService.update(id, updateUserDTO, req);
+  }
 }
