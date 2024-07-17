412825e24475ea76897dafd20fc375977db57761
Uday Kiran Kavaturu <udaykiran.k@zemosolabs.com>
2024-07-17 12:37:35 +0530

    chore: lint and format

--- src/contracts/contracts.service.ts
+++ src/contracts/contracts.service.ts
@@ -1,5 +1,10 @@
-import { ForbiddenException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { CreateContractDTO, UpdateContractDTO } from './dto';
+import {
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { CreateContractDTO } from './dto';
 import { Contract, UserType } from 'src/entities';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -9,9 +14,12 @@ export class ContractsService {
   constructor(
     @InjectRepository(Contract)
     private contractsRepository: Repository<Contract>,
-  ) { }
+  ) {}
 
-  async createContract(createContractDTO: CreateContractDTO, currentUserID: number) {
+  async createContract(
+    createContractDTO: CreateContractDTO,
+    currentUserID: number,
+  ) {
     const newContract =
       await this.contractsRepository.create(createContractDTO);
     newContract.created_by = currentUserID;
@@ -19,13 +27,25 @@ export class ContractsService {
     return await this.contractsRepository.save(newContract);
   }
 
-  async updateContract(id: number, contract: Partial<Contract>, currentUserID: number, currentUserType: string) {
-    const contractDetails = await this.contractsRepository.findOne({ where: { id } });
+  async updateContract(
+    id: number,
+    contract: Partial<Contract>,
+    currentUserID: number,
+    currentUserType: string,
+  ) {
+    const contractDetails = await this.contractsRepository.findOne({
+      where: { id },
+    });
     if (!contractDetails)
       throw new HttpException('contract not found', HttpStatus.NOT_FOUND);
 
-    if (contractDetails.lender_id != currentUserID && currentUserType != UserType.ADMIN)
-      throw new ForbiddenException('You can only view or update your own contract');
+    if (
+      contractDetails.lender_id != currentUserID &&
+      currentUserType != UserType.ADMIN
+    )
+      throw new ForbiddenException(
+        'You can only view or update your own contract',
+      );
 
     contract.updated_by = currentUserID;
     await this.contractsRepository.update(id, contract);
--- src/swagger/contracts.ts
+++ src/swagger/contracts.ts
@@ -15,31 +15,30 @@ export const CONTRACTS_SWAGGER_RESPONSES = {
   },
 
   updated: {
-    "id": 1,
-    "lender_id": null,
-    "name": "updating contract",
-    "type": "monthly",
-    "amount": "10000.00",
-    "fee": "12.50",
-    "status": 1,
-    "scheduled_due_date": 1,
-    "created_by": null,
-    "updated_by": 6,
-    "created_at": "2024-07-17T05:31:27.000Z",
-    "updated_at": "2024-07-17T06:50:46.000Z"
+    id: 1,
+    lender_id: null,
+    name: 'updating contract',
+    type: 'monthly',
+    amount: '10000.00',
+    fee: '12.50',
+    status: 1,
+    scheduled_due_date: 1,
+    created_by: null,
+    updated_by: 6,
+    created_at: '2024-07-17T05:31:27.000Z',
+    updated_at: '2024-07-17T06:50:46.000Z',
   },
 
   contractNotFound: {
-    "statusCode": 404,
-    "message": "contract not found",
-    "timestamp": "2024-07-17T07:04:42.255Z",
-    "path": "/contracts?id=20"
+    statusCode: 404,
+    message: 'contract not found',
+    timestamp: '2024-07-17T07:04:42.255Z',
+    path: '/contracts?id=20',
   },
   forbidden: {
-    "statusCode": 403,
-    "message": "You can only view or update your own contract",
-    "timestamp": "2024-07-17T07:04:58.615Z",
-    "path": "/contracts?id=1"
-  }
-
+    statusCode: 403,
+    message: 'You can only view or update your own contract',
+    timestamp: '2024-07-17T07:04:58.615Z',
+    path: '/contracts?id=1',
+  },
 };
