a937ae7580f68a2a3af0fec956f3f5d191cc0787
Uday Kiran Kavaturu <udaykiran.k@zemosolabs.com>
2024-07-18 15:21:50 +0530

    chore: added swagger documentation for create cash kick API

---
+++ src/swagger/cash-kicks.ts
@@ -0,0 +1,25 @@
+export const CASH_KICKS_SWAGGER_RESPONSES = {
+    created: {
+        "id": 21,
+        "seeker_id": 19,
+        "contract_id": 3,
+        "status": "pending",
+        "created_by": 19,
+        "updated_by": 19,
+        "created_at": "2024-07-18T07:28:58.000Z",
+        "updated_at": "2024-07-18T07:28:58.000Z",
+        "cash_kick_id": {
+            "id": 16,
+            "seeker_id": 19,
+            "name": "cask kick one",
+            "maturity_date": null,
+            "total_financed": "20000.25",
+            "total_received": "0.00",
+            "status": "pending",
+            "created_by": 19,
+            "updated_by": 19,
+            "created_at": "2024-07-18T07:28:58.000Z",
+            "updated_at": "2024-07-18T07:28:58.000Z"
+        }
+    }
+}
--- src/swagger/index.ts
+++ src/swagger/index.ts
@@ -1,3 +1,4 @@
 export * from './auth';
 export * from './common';
 export * from './users';
+export * from './cash-kicks';
